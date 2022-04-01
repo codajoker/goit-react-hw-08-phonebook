93335a27a3684b8dcf5156cbe749eb35e1ce6094
codajoker <codajoker91@gmail.com>
2022-04-01 12:40:56 +0200

    add Navigetion menu and style

---
+++ src/components/AppBar/AppBar.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const NavWrapperLinks = styled.div``;
+export const NavLinkRegister = styled(NavLink)``;
+export const NavLinkLogin = styled(NavLink)``;
