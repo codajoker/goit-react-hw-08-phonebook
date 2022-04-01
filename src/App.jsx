936a1a3aa965768efd9660cf9109a334ef13f274
codajoker <codajoker91@gmail.com>
2022-04-01 12:41:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,67 @@
-import { Section, Form, ListContact, Filter } from './components';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchItem, addItem } from './redux/operetions';
-import { useEffect } from 'react';
+// import { Section, Form, ListContact, Filter, MyContact } from './components';
+import { useSelector } from 'react-redux';
+import { token } from './redux/operetions';
+import { Route, Routes } from 'react-router-dom';
+
+// import { Navigation } from 'components/AppBar/AppBar';
+// import { FormRegister } from 'components/FormRegister/FormRegister';
+// import { FormLogin } from 'components/FormLogin/FormLogin';
+import { lazy, Suspense, useEffect } from 'react';
+import PrivateRoute from 'components/PrivateRoute/PrivateRout';
+import PublicRoute from 'components/PublicRoute/PublicRoute';
+const Section = lazy(() => import('./components/Section/Section'));
+const Navigation = lazy(() => import('./components/AppBar/AppBar'));
+
+const MyContact = lazy(() => import('./components/MyContact/MyContact'));
+const FormRegister = lazy(() =>
+  import('./components/FormRegister/FormRegister')
+);
+const FormLogin = lazy(() => import('./components/FormLogin/FormLogin'));
+
 function App() {
-  let contacts = useSelector(state => state.contacts.item);
-  const dispatch = useDispatch();
-  const filter = useSelector(state => state.contacts.filter);
+  const isFetchingCurrentUser = useSelector(
+    state => state.auth.isFetchingCurrentUser
+  );
+  const tokenUser = useSelector(state => state.auth.token);
   useEffect(() => {
-    dispatch(fetchItem());
-  }, []);
-
-  const addContact = data => {
-    if (
-      contacts.find(contact => {
-        return (
-          contact.name.toLocaleLowerCase() === data.name.toLocaleLowerCase()
-        );
-      })
-    ) {
-      return alert(`${data.name} is already in contacts`);
-    }
-    dispatch(addItem(data));
-  };
-
-  const findContact = () => {
-    const filterContact = contacts.filter(contact => {
-      return contact.name
-        .toLocaleLowerCase()
-        .includes(filter.toLocaleLowerCase());
-    });
-    return filterContact;
-  };
+    token.set(tokenUser);
+  }, [tokenUser]);
 
   return (
     <>
-      <Section title="Phonebook">
+      {!isFetchingCurrentUser && (
+        <Suspense fallback={''}>
+          <Navigation></Navigation>
+          <Routes>
+            <Route path="/" element={<Section />} />
+            <Route
+              path="register"
+              element={
+                <PublicRoute restricted>
+                  <FormRegister />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <PublicRoute restricted>
+                  <FormLogin />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="MyContact"
+              element={
+                <PrivateRoute>
+                  <MyContact />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
+      )}
+      {/* <Section title="Phonebook">
         <Form onSubmit={addContact} />
       </Section>
       <Section title="Contact">
@@ -44,7 +71,7 @@ function App() {
             <ListContact contacts={findContact()}></ListContact>
           </>
         )}
-      </Section>
+      </Section> */}
     </>
   );
 }
