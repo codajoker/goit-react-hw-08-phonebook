5a69b2de04d5740197282c29360addf4950bef9e
codajoker <codajoker91@gmail.com>
2022-04-01 12:43:55 +0200

    This is homePage

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,15 +1,61 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
+import { ToastContainer, toast } from 'react-toastify';
 
-export const Section = ({ title, children }) => {
+const Section = ({ children }) => {
+  const logIn = useSelector(state => state.auth.logIn);
+  const name = useSelector(state => state.auth.user.name);
+
+  useEffect(() => {
+    if (logIn) {
+      toast.info(
+        `
+         Привет 🦄${name} ты вошел в аккаунт и теперь можешь воспользоваться контактами `,
+        {
+          position: 'top-center',
+          autoClose: 6000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        }
+      );
+    }
+  }, [logIn, name]);
+
   return (
     <section>
-      <h2> {title} </h2>
+      {logIn ? (
+        <>
+          {' '}
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+          <h1> Hello</h1>
+        </>
+      ) : (
+        <h2 style={{ textAlign: 'center' }}>
+          {' '}
+          Приветствуем. Зарегистрируйтесь или войдите в аккаунт чтобы
+          пользоваться нашими услугами{' '}
+        </h2>
+      )}
+
       {children}
     </section>
   );
 };
 Section.propTypes = {
-  title: PropTypes.string.isRequired,
   children: PropTypes.node,
 };
+export default Section;
