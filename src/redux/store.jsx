e8e6173ae9ea13729fd99e3e85583ea9edd55150
codajoker <codajoker91@gmail.com>
2022-04-01 12:42:30 +0200

    add persist in project

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,47 @@
 // После
 import { configureStore } from '@reduxjs/toolkit';
-import combine from './redux-redesers';
+import { itemSlice, authSlice } from './redux-redesers';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+const persistConfigItem = {
+  key: 'item',
+  storage,
+};
+const persistedReducerItem = persistReducer(
+  persistConfigItem,
+  itemSlice.reducer
+);
+const persistConfiAuth = {
+  key: 'auth',
+  storage,
+  whitlist: ['token'],
+};
+const persistedReducerAuth = persistReducer(
+  persistConfiAuth,
+  authSlice.reducer
+);
 export const store = configureStore({
   reducer: {
-    contacts: combine,
+    auth: persistedReducerAuth,
+
+    contacts: persistedReducerItem,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
